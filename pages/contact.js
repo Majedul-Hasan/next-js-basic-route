ceac4d26a65216c6dfc478608ec503eca175e7c5
Majedul Hasan <77298488+Majedul-Hasan@users.noreply.github.com>
2022-05-08 23:46:34 +0600

    navigation

--- pages/contact.js
+++ pages/contact.js
@@ -1,8 +1,31 @@
 
+import {useRouter} from "next/router";
+
 
 const ContactPage = () => {
+
+    const router = useRouter()
+
+    const redirectUserToHome = ()=>{
+      //router.push('/')
+      router.push({
+        pathname: '/wheels/[id]/[color]/[type]',
+          query:{
+            id:1,
+            color: 'red',
+            type:'round'
+
+          }
+      })
+
+    }
+
+
+
+
   return (
   <>
+  <button onClick={redirectUserToHome}>Go back to home </button>
     <h1>Contact</h1>
   </>);
 };
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,43 @@
+import Link from "next/link";
 
 
 const Home = ()=> {
   return (
     <>
     <h1>hello world</h1>
+
+    <ul>
+      <li>
+        <Link href='/users'>
+          <a className="styledLink">users</a>
+        
+        
+         </Link>
+       </li>
+       
+       <li>
+        <Link href='/wheels/1/red/round'>
+          <a className="styledLink">go to wheels</a>
+        
+        
+         </Link>
+       </li>
+       <li>
+        <Link href={{
+          pathname: '/wheels/[id]/[color]/[type]',
+          query:{
+            id:1,
+            color: 'red',
+            type:'round'
+
+          }
+        }}>
+          <a className="styledLink">go to wheels obj</a>
+        
+        
+         </Link>
+       </li>
+    </ul>
     
     </>
   )
