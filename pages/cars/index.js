c8dabc94a20cdcf55ad6a611ddbfdd2ca0aa6bb1
Majedul Hasan <77298488+Majedul-Hasan@users.noreply.github.com>
2022-05-09 17:22:44 +0600

    styles

--- pages/cars/index.js
+++ pages/cars/index.js
@@ -1,9 +1,11 @@
+import style from './index.module.css'
+
 import React from "react";
 
 const CarsPage = () => {
   return (
       <>
-        <h1>Cars Page</h1>
+        <h1 className={style.red} >Cars Page</h1>
       
       </>
 
