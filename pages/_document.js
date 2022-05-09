db8d4490561c3a11e23588a835914140bcb48880
Majedul Hasan <77298488+Majedul-Hasan@users.noreply.github.com>
2022-05-09 17:44:18 +0600

    document

---
+++ pages/_document.js
@@ -0,0 +1,14 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+const Document = () => {
+  return (
+    <Html lang="en">
+      <Head />
+      <body style={{background: '#e0e0e0'}}>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
+export default Document
